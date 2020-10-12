83d9fde93ee605e556c18f4801f65ed9c678cafc
3x3cut0r <executor55@gmx.de>
2020-10-12 16:28:27 +0200

    Autobuild RPi4 Image with MPTCP Kernel

--- autobuild_rpi4_mptcp_kernel.bash
+++ autobuild_rpi4_mptcp_kernel.bash
@@ -2,13 +2,13 @@
 # !!! HIGHLY EXPERIMENTAL !!!
 # !!! USE AT YOUR OWN RISK !!!
 
+### ------------------------------------------------------------------------ ###
 ### GITHUB SECTION ###
 # GLOBAL CONFIG #
 GIT_USERNAME="3x3cut0r" # your git username
 GIT_EMAIL="executor55@gmx.de" # your git email
 # COMMIT HASHES #
-GIT_COMMIT_RPI_SHA1=106fa147d3daa58d2c1ae5f41a29d07036fe7d0a # "Linux 4.19.127" on https://github.com/raspberrypi/linux/commit/106fa147d3daa58d2c1ae5f41a29d07036fe7d0a
-GIT_COMMIT_MPTCP_SHA1=106fa147d3daa58d2c1ae5f41a29d07036fe7d0a # "Linux 4.19.127" on https://github.com/multipath-tcp/mptcp/commit/106fa147d3daa58d2c1ae5f41a29d07036fe7d0a
+GIT_COMMIT_SHA=106fa147d3daa58d2c1ae5f41a29d07036fe7d0a # "Linux 4.19.127" commit on https://github.com/raspberrypi/linux/commit/106fa147d3daa58d2c1ae5f41a29d07036fe7d0a
 # BRANCH #
 RPI_BRANCH="rpi-4.19.y" # the git branch from https://github.com/raspberrypi/linux which contains the GIT_COMMIT_RPI_SHA1
 MPTCP_BRANCH="mptcp_v0.95" # the git branch from https://github.com/multipath-tcp/mptcp which contains the GIT_COMMIT_MPTCP_SHA1
@@ -20,14 +20,19 @@ MPTCP_BRANCH="mptcp_v0.95" # the git branch from https://github.com/multipath-tc
 KERNEL="kernel7l"
 KERNEL_CONFIG="bcm2711_defconfig"
 
+### IMAGE SECTION ###
+# Raspberry Pi OS (32-bit) with desktop and recommended software    = "https://downloads.raspberrypi.org/raspios_full_armhf_latest"
+# Raspberry Pi OS (32-bit) with desktop                             = "https://downloads.raspberrypi.org/raspios_armhf_latest"
+# Raspberry Pi OS (32-bit) Lite                                     = "https://downloads.raspberrypi.org/raspios_lite_armhf_latest"
+RASPIOS_URL="https://downloads.raspberrypi.org/raspios_lite_armhf_latest"
+
 ### GLOBAL SECTION ###
 # WORKING DIRECTORY #
 WORKING_DIR="$HOME" # home directory is recommended to prevent write permission issues
 # CPU CORES #
 CPU_CORES_FOR_COMPILING=4 # number of cpu cores to use for compiling
 
-
-
+### ------------------------------------------------------------------------ ###
 ### START OF SCRIPT ###
 cd $WORKING_DIR
 
@@ -57,7 +62,7 @@ git config merge.renameLimit 999999
 # ADD MULTIPATH-TCP/MPTCP FROM GITHUB #
 git remote add mptcp https://github.com/multipath-tcp/mptcp.git
 git fetch mptcp
-git checkout -b rpi_mptcp $GIT_COMMIT_MPTCP_SHA1
+git checkout -b rpi_mptcp $GIT_COMMIT_SHA
 #git checkout -b rpi_mptcp origin/rpi-4.19.y
 
 # MERGE REPOSITORYS #
@@ -77,42 +82,42 @@ cd $WORKING_DIR/linux
 # MAKE #
 make mrproper
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- $KERNEL_CONFIG
-#make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- nconfig
+#sudo make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- nconfig
 cp .config .config.old
-sed -i 's/CONFIG_IPV6=m/CONFIG_IPV6=y/g' .config
+sed -i  's/CONFIG_IPV6=m/CONFIG_IPV6=y/g' .config
 INSLINE=$(grep -rnw '.config' -e 'CONFIG_TCP_CONG_BBR=m' | cut -d: -f 1)
-sed "$(expr $INSLINE + 1)iCONFIG_TCP_CONG_LIA=y" .config
-sed "$(expr $INSLINE + 2)iCONFIG_TCP_CONG_OLIA=y" .config
-sed "$(expr $INSLINE + 3)iCONFIG_TCP_CONG_WVEGAS=y" .config
-sed "$(expr $INSLINE + 4)iCONFIG_TCP_CONG_BALIA=y" .config
-sed "$(expr $INSLINE + 5)iCONFIG_TCP_CONG_MCTCPDESYNC=y" .config
+sed -i  "$(expr $INSLINE + 1)iCONFIG_TCP_CONG_LIA=y" .config
+sed -i  "$(expr $INSLINE + 2)iCONFIG_TCP_CONG_OLIA=y" .config
+sed -i  "$(expr $INSLINE + 3)iCONFIG_TCP_CONG_WVEGAS=y" .config
+sed -i  "$(expr $INSLINE + 4)iCONFIG_TCP_CONG_BALIA=y" .config
+sed -i  "$(expr $INSLINE + 5)iCONFIG_TCP_CONG_MCTCPDESYNC=y" .config
 INSLINE=$(grep -rnw '.config' -e 'CONFIG_DEFAULT_CUBIC=y' | cut -d: -f 1)
-sed "$(expr $INSLINE + 1)i# CONFIG_DEFAULT_LIA is not set" .config
-sed "$(expr $INSLINE + 2)i# CONFIG_DEFAULT_OLIA is not set" .config
-sed "$(expr $INSLINE + 3)i# CONFIG_DEFAULT_WVEGAS is not set" .config
-sed "$(expr $INSLINE + 4)i# CONFIG_DEFAULT_BALIA is not set" .config
-sed "$(expr $INSLINE + 5)i# CONFIG_DEFAULT_MCTCPDESYNC is not set" .config
+sed -i  "$(expr $INSLINE + 1)i# CONFIG_DEFAULT_LIA is not set" .config
+sed -i  "$(expr $INSLINE + 2)i# CONFIG_DEFAULT_OLIA is not set" .config
+sed -i  "$(expr $INSLINE + 3)i# CONFIG_DEFAULT_WVEGAS is not set" .config
+sed -i  "$(expr $INSLINE + 4)i# CONFIG_DEFAULT_BALIA is not set" .config
+sed -i  "$(expr $INSLINE + 5)i# CONFIG_DEFAULT_MCTCPDESYNC is not set" .config
 INSLINE=$(grep -rnw '.config' -e '# CONFIG_IPV6_SEG6_HMAC is not set' | cut -d: -f 1)
-sed "$(expr $INSLINE + 1)iCONFIG_MPTCP=y" .config
-sed "$(expr $INSLINE + 2)iCONFIG_MPTCP_PM_ADVANCED=y" .config
-sed "$(expr $INSLINE + 3)iCONFIG_MPTCP_FULLMESH=y" .config
-sed "$(expr $INSLINE + 4)iCONFIG_MPTCP_NDIFFPORTS=y" .config
-sed "$(expr $INSLINE + 5)iCONFIG_MPTCP_BINDER=y" .config
-sed "$(expr $INSLINE + 6)iCONFIG_MPTCP_NETLINK=y" .config
-sed "$(expr $INSLINE + 7)iCONFIG_DEFAULT_FULLMESH=y" .config
-sed "$(expr $INSLINE + 8)i# CONFIG_DEFAULT_NDIFFPORTS is not set" .config
-sed "$(expr $INSLINE + 9)i# CONFIG_DEFAULT_BINDER is not set" .config
-sed "$(expr $INSLINE + 10)i# CONFIG_DEFAULT_NETLINK is not set" .config
-sed "$(expr $INSLINE + 11)i# CONFIG_DEFAULT_DUMMY is not set" .config
-sed "$(expr $INSLINE + 12)iCONFIG_DEFAULT_MPTCP_PM="fullmesh"" .config
-sed "$(expr $INSLINE + 13)iCONFIG_MPTCP_SCHED_ADVANCED=y" .config
-sed "$(expr $INSLINE + 14)iCONFIG_MPTCP_BLEST=y" .config
-sed "$(expr $INSLINE + 15)iCONFIG_MPTCP_ROUNDROBIN=y" .config
-sed "$(expr $INSLINE + 16)iCONFIG_MPTCP_REDUNDANT=y" .config
-sed "$(expr $INSLINE + 17)iCONFIG_DEFAULT_SCHEDULER=y" .config
-sed "$(expr $INSLINE + 18)i# CONFIG_DEFAULT_ROUNDROBIN is not set" .config
-sed "$(expr $INSLINE + 19)i# CONFIG_DEFAULT_REDUNDANT is not set" .config
-sed "$(expr $INSLINE + 20)iCONFIG_DEFAULT_MPTCP_SCHED="default"" .config
+sed -i  "$(expr $INSLINE + 1)iCONFIG_MPTCP=y" .config
+sed -i  "$(expr $INSLINE + 2)iCONFIG_MPTCP_PM_ADVANCED=y" .config
+sed -i  "$(expr $INSLINE + 3)iCONFIG_MPTCP_FULLMESH=y" .config
+sed -i  "$(expr $INSLINE + 4)iCONFIG_MPTCP_NDIFFPORTS=y" .config
+sed -i  "$(expr $INSLINE + 5)iCONFIG_MPTCP_BINDER=y" .config
+sed -i  "$(expr $INSLINE + 6)iCONFIG_MPTCP_NETLINK=y" .config
+sed -i  "$(expr $INSLINE + 7)iCONFIG_DEFAULT_FULLMESH=y" .config
+sed -i  "$(expr $INSLINE + 8)i# CONFIG_DEFAULT_NDIFFPORTS is not set" .config
+sed -i  "$(expr $INSLINE + 9)i# CONFIG_DEFAULT_BINDER is not set" .config
+sed -i  "$(expr $INSLINE + 10)i# CONFIG_DEFAULT_NETLINK is not set" .config
+sed -i  "$(expr $INSLINE + 11)i# CONFIG_DEFAULT_DUMMY is not set" .config
+sed -i  "$(expr $INSLINE + 12)iCONFIG_DEFAULT_MPTCP_PM=\"fullmesh\"" .config
+sed -i  "$(expr $INSLINE + 13)iCONFIG_MPTCP_SCHED_ADVANCED=y" .config
+sed -i  "$(expr $INSLINE + 14)iCONFIG_MPTCP_BLEST=y" .config
+sed -i  "$(expr $INSLINE + 15)iCONFIG_MPTCP_ROUNDROBIN=y" .config
+sed -i  "$(expr $INSLINE + 16)iCONFIG_MPTCP_REDUNDANT=y" .config
+sed -i  "$(expr $INSLINE + 17)iCONFIG_DEFAULT_SCHEDULER=y" .config
+sed -i  "$(expr $INSLINE + 18)i# CONFIG_DEFAULT_ROUNDROBIN is not set" .config
+sed -i  "$(expr $INSLINE + 19)i# CONFIG_DEFAULT_REDUNDANT is not set" .config
+sed -i  "$(expr $INSLINE + 20)iCONFIG_DEFAULT_MPTCP_SCHED=\"default\"" .config
 
 # COMPILE #
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j $CPU_CORES_FOR_COMPILING zImage
@@ -120,38 +125,44 @@ make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j $CPU_CORES_FOR_COMPILING mod
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j $CPU_CORES_FOR_COMPILING dtbs
 
 # WGET RASPIAN IMAGE #
-wget https://downloads.raspberrypi.org/raspbian_lite_latest
-unzip raspbian_lite_latest
-mv *.img raspbian.img
-fdisk -l raspbian.img
-mkdir -p mnt/fat32
-mkdir mnt/ext4
-START_PART_1=$(fdisk -l raspbian.img | grep raspbian.img1 | awk '{gsub(/[ ]+/," ")}1' | cut -d ' ' -f 2)
-START_PART_2=$(fdisk -l raspbian.img | grep raspbian.img2 | awk '{gsub(/[ ]+/," ")}1' | cut -d ' ' -f 2)
-UNITS=$(fdisk -l raspbian.img | grep Units | cut -d = -f 2 | cut -d ' ' -f 2 | cut -d ' ' -f 1)
-OFFSET_PART_1=$(expr $START_PART_1 \* $UNITS)
-OFFSET_PART_2=$(expr $START_PART_2 \* $UNITS)
+wget $RASPIOS_URL
+sudo chmod 755 $WORKING_DIR/linux/raspios_*
+unzip $WORKING_DIR/linux/raspios_* -d $WORKING_DIR/linux
+mv $WORKING_DIR/linux/*raspios*.img $WORKING_DIR/linux/raspios.img
+fdisk -l $WORKING_DIR/linux/raspios.img
+mkdir -p $WORKING_DIR/linux/mnt/fat32
+mkdir $WORKING_DIR/linux/mnt/ext4
+START_PART_1=$(fdisk -l $WORKING_DIR/linux/raspios.img | grep raspios.img1 | awk '{gsub(/[ ]+/," ")}1' | cut -d ' ' -f 2 | cut -d ' ' -f 1)
+START_PART_2=$(fdisk -l $WORKING_DIR/linux/raspios.img | grep raspios.img2 | awk '{gsub(/[ ]+/," ")}1' | cut -d ' ' -f 2 | cut -d ' ' -f 1)
+UNITS=$(fdisk -l $WORKING_DIR/linux/raspios.img | grep 'Units\|Einheiten' | cut -d = -f 2 | cut -d ' ' -f 2 | cut -d ' ' -f 1)
+OFFSET_PART_1=$(expr "$START_PART_1" '*' "$UNITS")
+OFFSET_PART_2=$(expr "$START_PART_2" '*' "$UNITS")
 
 # MOUNT IMAGE #
-sudo mount -v -o offset=$OFFSET_PART_2 -t ext4 raspbian.img mnt/ext4
-sudo make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_MOD_PATH=mnt/ext4 -j $CPU_CORES_FOR_COMPILING modules_install | tee output.txt
-VERSION=$(tail -n 1 output.txt  | awk '{gsub(/[ ]+/," ")}1' | cut -d ' ' -f 3)
-umount mnt/ext4
-mount -v -o offset=$OFFSET_PART_1 -t vfat raspbian.img mnt/fat32
-cp mnt/fat32/$KERNEL.img mnt/fat32/$KERNEL-backup.img
-cp arch/arm/boot/zImage mnt/fat32/$KERNEL.img
-cp arch/arm/boot/dts/*.dtb mnt/fat32/
-cp arch/arm/boot/dts/overlays/*.dtb* mnt/fat32/overlays/
-cp arch/arm/boot/dts/overlays/README mnt/fat32/overlays/
-echo "kernel=$KERNEL.img" >> mnt/fat32/config.txt
+mount -v -o offset=$OFFSET_PART_2 -t ext4 $WORKING_DIR/linux/raspios.img $WORKING_DIR/linux/mnt/ext4
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_MOD_PATH=$WORKING_DIR/linux/mnt/ext4 -j $CPU_CORES_FOR_COMPILING modules_install | tee /tmp/INSTALL_MOD_OUTPUT.txt
+VERSION=$(sudo tail -n 1 /tmp/INSTALL_MOD_OUTPUT.txt  | awk '{gsub(/[ ]+/," ")}1' | cut -d ' ' -f 3)
+umount $WORKING_DIR/linux/mnt/ext4
+mount -v -o offset=$OFFSET_PART_1 -t vfat $WORKING_DIR/linux/raspios.img $WORKING_DIR/linux/mnt/fat32
+cp $WORKING_DIR/linux/mnt/fat32/$KERNEL.img $WORKING_DIR/linux/mnt/fat32/$KERNEL-backup.img
+cp $WORKING_DIR/linux/arch/arm/boot/zImage $WORKING_DIR/linux/mnt/fat32/$KERNEL.img
+cp $WORKING_DIR/linux/arch/arm/boot/dts/*.dtb $WORKING_DIR/linux/mnt/fat32/
+cp $WORKING_DIR/linux/arch/arm/boot/dts/overlays/*.dtb* $WORKING_DIR/linux/mnt/fat32/overlays/
+cp $WORKING_DIR/linux/arch/arm/boot/dts/overlays/README $WORKING_DIR/linux/mnt/fat32/overlays/
+echo "kernel=$KERNEL.img" | tee -a $WORKING_DIR/linux/mnt/fat32/config.txt
+
 # UNMOUNT IMAGE #
-umount mnt/fat32
-mv raspbian.img "$WORKING_DIR"/Raspbian_RPi4_"$VERSION"_"$MPTCP_BRANCH".img
+umount $WORKING_DIR/linux/mnt/fat32
+mv $WORKING_DIR/linux/raspios.img "$WORKING_DIR"/RaspiOS_RPi4_"$VERSION"_"$MPTCP_BRANCH".img
 
 ### CLEANING UP AGAIN ###
 cd $WORKING_DIR
-sudo rm -rf $WORKING_DIR/linux
-sudo rm -rf $WORKING_DIR/tools
-sudo rm -rf .gitconfig
+sudo chown -R $(whoami):$(whoami) $WORKING_DIR/RaspiOS*
+sudo chmod 755 $WORKING_DIR/RaspiOS*
+ls -la RaspiOS*
+rm -rf $WORKING_DIR/linux
+rm -rf $WORKING_DIR/tools
+rm -rf $WORKING_DIR/.gitconfig
+rm -rf /tmp/INSTALL_MOD_OUTPUT.txt
 
 # DONE #
